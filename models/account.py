8ee9d608a89a466602aa2b6f6409a74aaf33b4a4
Nay3li <nayeli.valencia@xmarts.com.mx>
2017-11-20 17:01:41 -0600

    ajuste de comisiones

---
+++ models/account.py
@@ -0,0 +1,53 @@
+## -*- coding: utf-8 -*-
+
+from openerp import models, fields, api, _, tools
+from openerp.exceptions import UserError, RedirectWarning, ValidationError
+import xlrd
+import shutil
+import logging
+_logger = logging.getLogger(__name__)
+
+class AccountInvoiceLIne(models.Model):
+    _inherit ='account.invoice.line'
+    costo= fields.Float(string='Costo')
+    @api.model
+    def create(self,vals):
+        producto = self.env['product.product'].search([('id','=',vals['product_id'])])
+        costo=producto.standard_price
+        total=  vals['price_unit'] - costo
+        vals['costo']=total * vals['quantity']
+        return super(AccountInvoiceLIne,self).create(vals)
+class AccountInvoice(models.Model):
+    _inherit='account.invoice'
+    ganancy= fields.Float(string="ganancia", store=True, compute="_compute_ganancy")
+    @api.depends('invoice_line_ids')
+    @api.one
+    def _compute_ganancy(self):
+        total=0
+        for i in self.invoice_line_ids:
+            total += i.costo
+        self.ganancy = total
+
+class AccountPayment(models.Model):
+    _inherit ='account.payment'
+    @api.multi
+    def get_teamwise_commission(self):
+        sum_line_manager = []
+        sum_line_person = []
+        amount_person, amount_manager = 0.0, 0.0
+        for payment in self:
+            if not payment.sales_team_id:
+                raise Warning(_('Plaese select Sales Team.'))
+            if not payment.sales_user_id:
+                raise Warning(_('Plaese select Sales User.'))
+            if payment.invoice_ids:
+                for invoice in payment.invoice_ids:
+                    ##modificacion naye
+                    sum_line_manager.append((invoice.ganancy * invoice.team_id.sales_manager_commission)/100)
+                    sum_line_person.append((invoice.ganancy * invoice.team_id.sales_person_commission)/100)
+                amount_manager = sum(sum_line_manager)
+                amount_person = sum(sum_line_person)
+            else:
+                amount_manager = (invoice.ganancy * payment.sales_team_id.sales_manager_commission)/100
+                amount_person =  (invoice.ganancy * payment.sales_team_id.sales_person_commission)/100
+        return amount_person, amount_manager
