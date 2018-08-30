e16cf9a9e8f04c66177bf700c3c6421f1ed0cb29
Unknown <williamcolin527@gmail.com>
2018-08-30 01:32:19 -0500

    Ajustes de factura 2.2

--- models/models.py
+++ models/models.py
@@ -4,9 +4,15 @@ from openerp import models, fields, api, _, tools
 from openerp.exceptions import UserError, RedirectWarning, ValidationError
 import xlrd
 import shutil
-import logging
-_logger = logging.getLogger(__name__)
 
 #class xmartsstockpicking(models.Model):
 #	_inherit ='stock.picking'
 #  	xmpack= fields.Boolean(string="Empaquetado",default=False)
+
+
+class InheritZona(models.Model):
+
+	_inherit = 'res.partner'
+
+	zone = fields.Char(string='Zona de Residencia', help='Coloca la zona de residencia para especificar la zona de localizacion')
+
